f385619ce8826029b27c78dc8e876e810f3152ac
Collinb190 <collinb190@gmail.com>
2024-01-21 23:04:54 -0600

    update acronym

--- python/acronym.py
+++ python/acronym.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-Creates an alias
+Creates an acronym
 """
 
-# Enter the string to create an acrnym
-x = input("Enter the string to create an acrnym\n")
-# Remove the white space from the string
+# Get string/ Remove whitespace/ String to list/ Hold acronym
+x = input("Enter the string to create an acronym\n")
 x = x.strip()
-# Turn the string into a list
-y = x.split() 
-# Get the first char of each string in the list
+y = x.split()
 a = ""
+
+# Get the first char of each string in the list
 for word in y:
     a += word[0].capitalize()
 print(a)
